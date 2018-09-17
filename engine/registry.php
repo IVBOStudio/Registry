64d4cf78fe2fde2103fbef1ccc71e297c5ebf2a4
Pro808 <32733438+Pro808@users.noreply.github.com>
2018-09-17 13:25:22 +0400

    Первая версия аутолоадера и конфига

--- engine/registry.php
+++ engine/registry.php
@@ -1,47 +1,8 @@
 <?php
-
+include "autoLoaderClass.php";
 
 class registry
 {
-    function lock()
-    {
-
-    }
-
-    function reg()
-    {
-
-    }
-
-    function sOSI()
-    {
-
-    }
-
-    function AndreyHello()
-    {
-
-    }
-
-    function GGGG()
-    {
-
-    }
-
-    function nik222()
-    {
-
-    }
-
-    function nik()
-    {
-
-    }
-
-    function nik22233334444555()
-    {
-
-    }
 
 
 }
